693ea965d2de54d89e378eda843dab25372e3ef2
Tisha Dobariya <tishadobariya8@gmail.com>
2026-07-18 01:02:04 +0530

    Add updated requirements

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
     baseURL: "https://nexora-backend-55zy.onrender.com",
+    // baseURL: "http://127.0.0.1:8000",
 });
 
 export default API;
